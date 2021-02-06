283fcf2ef25b26597c91ca2e0d7cdbce66cf274a
BodNS <bondarenokn@gmail.com>
2021-02-06 11:08:13 +0200

    add. KNOPKA and RICHAG

--- assets/js/index2.js
+++ assets/js/index2.js
@@ -1,22 +1,32 @@
-const userCard = document.createElement("div");
-userCard.classList.add("userCard");
+/* KNOPKA 
+document.getElementById('id1').onclick=function (){
+    //const button_id2=document.getElementById('id2');
+    //button_id2.hidden = true
+    document.getElementById('id2').hidden = true; //tak koroche
+}*/
 
-const userCardElem = getElementByClassName("userCard");
-const userPhotoContainer = document.createElement("div");
-userPhotoContainer.classList.add("userPhoto");
+//"RICHAG"
+let slider = document.querySelector('.slider');
+let thumb = document.querySelector('.thumb');
+thumb.onmousedown = function(event){
+    event.preventDefault();
+    let newShiftX = event.clientX - thumb.getBoundingClientRect().left;
+    document.addEventListener('mousemove', onMouseMoveAction);
+    document.addEventListener('mouseup', onMouseUpAction);
 
-const imageElem = document.createElement("img");
-imageElem.setAttribute("src", card.imageSrc);
-imageElem.setAttribute("alt", "userPhoto");
+    function onMouseMoveAction(event){
+        let leftPosition = event.clientX - newShiftX - slider.getBoundingClientRect().left;
+        if(leftPosition<0) leftPosition=0;
+        let rightPosition = slider.offsetWidth - thumb.offsetWidth;
+        if(leftPosition > rightPosition) leftPosition=rightPosition;
+        thumb.style.left = leftPosition + 'px';
+        console.log('left:', leftPosition, 'right:', rightPosition);
+        
+    }
 
-userPhotoContainer.append(imageElem);
-
-userCardElem.append(userPhotoContainer);
-
-const userStats = document.createElement("div");
-userStats.classList.add("userStats");
-
-const userStatValue = document.createElement("div");
-userStatValue.classList.add("userStatValue");
-userCardElem.append(userStatValue);
+    function onMouseUpAction(){
+        document.removeEventListener('mouseup', onMouseUpAction);
+        document.removeEventListener('mousemove', onMouseMoveAction);
+    }
+}
 
